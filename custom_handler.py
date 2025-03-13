9915aff9afcde8b0dd98354eb738b9eaca2df93c
Kirill Igumenshchev <kirill.igum@gmail.com>
2025-03-13 10:27:01 -0700

    refactor: pop messages and remove model param from kwargs

--- custom_handler.py
+++ custom_handler.py
@@ -4,12 +4,13 @@ from litellm import CustomLLM, completion, get_llm_provider
 
 class MyCustomLLM(CustomLLM):
     def completion(self, *args, **kwargs) -> litellm.ModelResponse:
+        messages = kwargs.pop(
+            "messages", None
+        )  # Remove "messages" from kwargs and store it
+        kwargs.pop("model", None)  # Remove the incoming model value
         return litellm.completion(
-            model="gpt-4o-mini",
-            # model="groq/llama-3.3-70b-specdec",
-            # model="g33",
-            # messages=[{"role": "user", "content": "Hello world"}],
-            # mock_response="Hi!",
+            model="gpt-4o-mini",  # Override model to gpt-4o-mini as required
+            messages=messages,
         )  # type: ignore
 
     async def acompletion(self, *args, **kwargs) -> litellm.ModelResponse:
@@ -20,7 +21,6 @@ class MyCustomLLM(CustomLLM):
         return litellm.completion(
             model="gpt-4o-mini",  # Override model to gpt-4o-mini as required
             messages=messages,
-            # **kwargs,
         )  # type: ignore
 
 
